8554b26b659ae406b3ecbb8d114555f56790375f
N0mekop <blinkevin@hotmail.com>
2022-07-15 14:18:19 +0200

    Vehicle, Car, Byclicle added and updated

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
-require_once 'car.php';
-require_once 'bike.php';
+require_once 'Car.php';
+require_once 'Bicycle.php';
 
-$carOne = new Car('Yellow');
+$bicycle = new Bicycle('blue', 1);
+echo $bicycle->forward();
+var_dump($bicycle);
+
+$carOne = new Car('Yellow', 4, 'Gasoline');
 
 echo $carOne->start();
 echo '<br>' . $carOne->forward();
@@ -16,18 +20,18 @@ echo '<br> Seats: ' . $carOne->getNbSeats();
 echo '<br> Fuel: ' . $carOne->getEnergy();
 echo '<br> Fuel Level: ' . $carOne->getEnergyLevel();
 
-$bike = new Bike("Black"); // Instanciation d'un nouvel objet $bike de la classe Bicycle
+$bicycle = new Bicycle("Black", 1);
 
-$bike->setColor("Yellow");
-$bike->getCurrentSpeed();
+$bicycle->setColor("Yellow");
+$bicycle->getCurrentSpeed();
 
-echo '<br> <br>' . $bike->forward();
-echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
-echo $bike->brake();
-echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
-echo $bike->brake();
+echo '<br> <br>' . $bicycle->forward();
+echo '<br> Vitesse du vélo : ' . $bicycle->getCurrentSpeed() . ' km/h' . '<br>';
+echo $bicycle->brake();
+echo '<br> Vitesse du vélo : ' . $bicycle->getCurrentSpeed() . ' km/h' . '<br>';
+echo $bicycle->brake();
 
-$carTwo = new Car('Black');
+$carTwo = new Car('Black', 4, 'Gasoline');
 
 echo '<br> <br>' . $carTwo->start();
 echo '<br>' . $carTwo->forward();
